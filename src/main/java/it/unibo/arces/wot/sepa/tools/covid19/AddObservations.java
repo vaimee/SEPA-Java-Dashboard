246401c83a34249799cec2c4456031cfb840de98
Luca Roffia <lroffia@gmail.com>
2020-04-01 14:30:03 +0200

    Ready to start updating also national data

--- src/main/java/it/unibo/arces/wot/sepa/tools/covid19/AddObservations.java
+++ src/main/java/it/unibo/arces/wot/sepa/tools/covid19/AddObservations.java
@@ -1,5 +1,6 @@
 package it.unibo.arces.wot.sepa.tools.covid19;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,7 +9,9 @@ import java.util.HashMap;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPABindingsException;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPAPropertiesException;
@@ -102,12 +105,14 @@ public class AddObservations extends Producer {
 		properties.put("terapia_intensiva", "covid19:IntensiveCare");
 		properties.put("totale_ospedalizzati", "covid19:TotalHospitalised");
 		properties.put("isolamento_domiciliare", "covid19:HomeConfinement");
-		properties.put("totale_attualmente_positivi", "covid19:TotalPositiveCases");
-		properties.put("nuovi_attualmente_positivi", "covid19:DailyPositiveCases");
 		properties.put("dimessi_guariti", "covid19:Recovered");
 		properties.put("deceduti", "covid19:Death");
 		properties.put("totale_casi", "covid19:TotalCases");
 		properties.put("tamponi", "covid19:TestPerformed");
+		
+		properties.put("totale_positivi", "covid19:TotalPositiveCases");
+		properties.put("nuovi_positivi", "covid19:DailyPositiveCases");
+		properties.put("variazione_totale_positivi", "covid19:DeltaTotalPositiveCases");
 	}
 	
 	public AddObservations(String hostFile) throws SEPAProtocolException, SEPASecurityException, SEPAPropertiesException, FileNotFoundException, IOException {
@@ -117,58 +122,264 @@ public class AddObservations extends Producer {
 	public AddObservations() throws SEPAProtocolException, SEPASecurityException, SEPAPropertiesException, FileNotFoundException, IOException {
 		this(null,null);
 	}
-
-	public void addRegionObservations(String graph) throws FileNotFoundException, SEPABindingsException, SEPASecurityException, SEPAProtocolException, SEPAPropertiesException {
-		JsonArray array = loadJsonArray("dpc-covid19-ita-regioni-latest.json");
+	
+	public void addNationalObservations(String graph,JsonArray array) throws FileNotFoundException, SEPABindingsException, SEPASecurityException, SEPAProtocolException, SEPAPropertiesException {
+		/*
+		 *     {
+        "data": "2020-03-31T17:00:00",
+        "stato": "ITA",
+        "ricoverati_con_sintomi": 28192,
+        "terapia_intensiva": 4023,
+        "totale_ospedalizzati": 32215,
+        "isolamento_domiciliare": 45420,
+        "totale_positivi": 77635,
+        "variazione_totale_positivi": 2107,
+        "nuovi_positivi": 4053,
+        "dimessi_guariti": 15729,
+        "deceduti": 12428,
+        "totale_casi": 105792,
+        "tamponi": 506968,
+        "note_it": "",
+        "note_en": ""
+		 * 
+		 * */
+		
+//		JsonArray array = loadJsonArray("dpc-covid19-ita-andamento-nazionale-latest.json");
 		
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("ricoverati_con_sintomi");
 		list.add("terapia_intensiva");
 		list.add("totale_ospedalizzati");
 		list.add("isolamento_domiciliare");
-		list.add("totale_attualmente_positivi");
-		list.add("nuovi_attualmente_positivi");
+		list.add("totale_positivi");
+		list.add("nuovi_positivi");
 		list.add("dimessi_guariti");
 		list.add("deceduti");
 		list.add("totale_casi");
 		list.add("tamponi");
+		list.add("variazione_totale_positivi");
+		
+		setUpdateBindingValue("unit", new RDFTermURI("unit:Number"));
+		setUpdateBindingValue("graph", new RDFTermURI(graph));
+		
+		for (JsonElement country : array) {
+			String placeURIString = new String("http://covid19/context/country/" +country.getAsJsonObject().get("stato").getAsString().replace(" ", "_").replace("'", "_"));
+			
+			RDFTermURI place = new RDFTermURI(placeURIString);
+			RDFTermLiteral timestamp = new RDFTermLiteral(country.getAsJsonObject().get("data").getAsString().replace(" ", "T").concat("Z"));
+			
+			setUpdateBindingValue("place",place );
+			setUpdateBindingValue("timestamp", timestamp);
+			
+			for (String pro : list) {
+				RDFTermURI property = new RDFTermURI(properties.get(pro));
+				RDFTermLiteral value = new RDFTermLiteral(String.valueOf(country.getAsJsonObject().get(pro).getAsInt()));
+				
+				setUpdateBindingValue("property", property);			
+				setUpdateBindingValue("value", value);	
+			
+				logger.info("Country: "+place+ " Property: "+property+" Value: " +value+ " Timestamp: "+timestamp); 
+				
+				update();
+			}	
+		}
+	}
+
+	public void addRegionObservations(String graph,JsonArray array) throws FileNotFoundException, SEPABindingsException, SEPASecurityException, SEPAProtocolException, SEPAPropertiesException {
+		/*		 
+		 * "totale_attualmente_positivi" ==> "totale_positivi"
+		 * "nuovi_attualmente_positivi" ==> "nuovi_positivi" 
+		 * 
+		 * NUOVA PROPRIETA'
+		 * "variazione_totale_positivi" ???
+		 * 
+		 * 
+		 * "totale_positivi":1191,
+		 * "variazione_totale_positivi":22,
+		 * "nuovi_positivi":56,
+		 * 
+		 * */
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("ricoverati_con_sintomi");
+		list.add("terapia_intensiva");
+		list.add("totale_ospedalizzati");
+		list.add("isolamento_domiciliare");
+		list.add("totale_positivi");
+		list.add("nuovi_positivi");
+		list.add("dimessi_guariti");
+		list.add("deceduti");
+		list.add("totale_casi");
+		list.add("tamponi");
+		list.add("variazione_totale_positivi");
 		
 		setUpdateBindingValue("unit", new RDFTermURI("unit:Number"));
 		setUpdateBindingValue("graph", new RDFTermURI(graph));
 		
 		for (JsonElement prov : array) {
-			setUpdateBindingValue("place", new RDFTermURI("http://covid19/Italy/Region/" +prov.getAsJsonObject().get("denominazione_regione").getAsString().replace(" ", "_").replace("'", "_")));
-			setUpdateBindingValue("timestamp", new RDFTermLiteral(prov.getAsJsonObject().get("data").getAsString().replace(" ", "T").concat("Z")));
+			String placeURIString = new String("http://covid19/Italy/Region/" +prov.getAsJsonObject().get("denominazione_regione").getAsString().replace(" ", "_").replace("'", "_"));
+			
+			// FIX
+			if (placeURIString.endsWith("Emilia-Romagna")) placeURIString = placeURIString.replace("-", "_");
+			
+			RDFTermURI place = new RDFTermURI(placeURIString);
+			RDFTermLiteral timestamp = new RDFTermLiteral(prov.getAsJsonObject().get("data").getAsString().replace(" ", "T").concat("Z"));
+			
+			setUpdateBindingValue("place",place );
+			setUpdateBindingValue("timestamp", timestamp);
 			
 			for (String pro : list) {
-				setUpdateBindingValue("property", new RDFTermURI(properties.get(pro)));			
-				setUpdateBindingValue("value", new RDFTermLiteral(String.valueOf(prov.getAsJsonObject().get(pro).getAsInt())));	
+				RDFTermURI property = new RDFTermURI(properties.get(pro));
+				RDFTermLiteral value = new RDFTermLiteral(String.valueOf(prov.getAsJsonObject().get(pro).getAsInt()));
+				
+				setUpdateBindingValue("property", property);			
+				setUpdateBindingValue("value", value);	
 			
+				logger.info("Region: "+place+ " Property: "+property+" Value: " +value+ " Timestamp: "+timestamp); 
+				
 				update();
 			}	
 		}
 	}
 	
-	public void addProvinceObservations(String graph) throws FileNotFoundException, SEPABindingsException, SEPASecurityException, SEPAProtocolException, SEPAPropertiesException {
-		JsonArray array = loadJsonArray("dpc-covid19-ita-province-latest.json");
-		
+	public void addProvinceObservations(String graph,JsonArray array) throws FileNotFoundException, SEPABindingsException, SEPASecurityException, SEPAProtocolException, SEPAPropertiesException {	
 		setUpdateBindingValue("unit", new RDFTermURI("unit:Number"));
 		setUpdateBindingValue("graph", new RDFTermURI(graph));
 		
 		for (JsonElement prov : array) {
-			setUpdateBindingValue("place", new RDFTermURI("http://covid19/Italy/Province/" + prov.getAsJsonObject().get("denominazione_provincia").getAsString().replace(" ", "_").replace("'", "_")));			
-			setUpdateBindingValue("timestamp", new RDFTermLiteral(prov.getAsJsonObject().get("data").getAsString().replace(" ", "T").concat("Z")));
+			RDFTermURI place = new RDFTermURI("http://covid19/Italy/Province/" + prov.getAsJsonObject().get("denominazione_provincia").getAsString().replace(" ", "_").replace("'", "_"));
+			RDFTermLiteral timestamp = new RDFTermLiteral(prov.getAsJsonObject().get("data").getAsString().replace(" ", "T").concat("Z"));
+			RDFTermURI property = new RDFTermURI(properties.get("totale_casi"));
+			RDFTermLiteral value =  new RDFTermLiteral(String.valueOf(prov.getAsJsonObject().get("totale_casi").getAsInt()));
 			
-			setUpdateBindingValue("property", new RDFTermURI(properties.get("totale_casi")));
-			setUpdateBindingValue("value", new RDFTermLiteral(String.valueOf(prov.getAsJsonObject().get("totale_casi").getAsInt())));
+			setUpdateBindingValue("place", place);			
+			setUpdateBindingValue("timestamp", timestamp);
+			
+			setUpdateBindingValue("property", property );
+			setUpdateBindingValue("value", value);
+			
+			logger.info("Province: "+place+ " Property: "+property+" Value: " +value+ " Timestamp: "+timestamp);
 			
 			update();
 		}
 	}
 	
-	public JsonArray loadJsonArray(String jsapFile) throws FileNotFoundException {
+	public static JsonArray loadJsonArray(String jsapFile) throws FileNotFoundException {
 		FileReader in = new FileReader(jsapFile);
 		
 		return new JsonParser().parse(in).getAsJsonArray();
+	}
+	
+	public static JsonArray loadCSVProvince(String csv) throws IOException {
+		FileReader in = new FileReader(csv);
+
+		String line;
+
+		BufferedReader br = new BufferedReader(in);
+
+		// Skip first line
+		br.readLine();
+
+		JsonArray ret = new JsonArray();
+		while ((line = br.readLine()) != null) {
+			String[] fields = line.split(",");
+
+			JsonElement elem = new JsonObject();
+			try {
+				elem.getAsJsonObject().add("denominazione_provincia", new JsonPrimitive(fields[5]));
+				elem.getAsJsonObject().add("data", new JsonPrimitive(fields[0]));
+				elem.getAsJsonObject().add("totale_casi", new JsonPrimitive(fields[9]));
+				ret.add(elem);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				logger.error(e.getMessage());
+				logger.error("Wrong line: "+line);
+			}
+			
+		}
+
+		br.close();
+
+		return ret;
+	}
+
+	public static JsonArray loadCSVRegioni(String csv) throws IOException {
+		FileReader in = new FileReader(csv);
+
+		String line;
+
+		BufferedReader br = new BufferedReader(in);
+
+		// Skip first line
+		br.readLine();
+
+		// data,stato,codice_regione,denominazione_regione,lat,long,
+		// 6
+		// ricoverati_con_sintomi,terapia_intensiva,totale_ospedalizzati,isolamento_domiciliare,totale_attualmente_positivi,nuovi_attualmente_positivi,dimessi_guariti,deceduti,totale_casi,tamponi
+		// note_it,note_en
+		JsonArray ret = new JsonArray();
+		while ((line = br.readLine()) != null) {
+			String[] fields = line.split(",");
+
+			JsonElement elem = new JsonObject();
+			elem.getAsJsonObject().add("denominazione_regione", new JsonPrimitive(fields[3]));
+			elem.getAsJsonObject().add("data", new JsonPrimitive(fields[0]));
+
+			elem.getAsJsonObject().add("ricoverati_con_sintomi", new JsonPrimitive(fields[6]));
+			elem.getAsJsonObject().add("terapia_intensiva", new JsonPrimitive(fields[7]));
+			elem.getAsJsonObject().add("totale_ospedalizzati", new JsonPrimitive(fields[8]));
+			elem.getAsJsonObject().add("isolamento_domiciliare", new JsonPrimitive(fields[9]));
+			elem.getAsJsonObject().add("totale_attualmente_positivi", new JsonPrimitive(fields[10]));
+			elem.getAsJsonObject().add("nuovi_attualmente_positivi", new JsonPrimitive(fields[11]));
+			elem.getAsJsonObject().add("dimessi_guariti", new JsonPrimitive(fields[12]));
+			elem.getAsJsonObject().add("deceduti", new JsonPrimitive(fields[13]));
+			elem.getAsJsonObject().add("totale_casi", new JsonPrimitive(fields[14]));
+			elem.getAsJsonObject().add("tamponi", new JsonPrimitive(fields[15]));
+
+			ret.add(elem);
+		}
+
+		br.close();
+
+		return ret;
+	}
+	
+	public static JsonArray loadCSVNazionale(String csv) throws IOException {
+		FileReader in = new FileReader(csv);
+
+		String line;
+
+		BufferedReader br = new BufferedReader(in);
+
+		// Skip first line
+		br.readLine();
+
+		//data,stato,ricoverati_con_sintomi,terapia_intensiva,totale_ospedalizzati,isolamento_domiciliare,totale_positivi,variazione_totale_positivi,nuovi_positivi,dimessi_guariti,deceduti,totale_casi,tamponi,note_it,note_en
+		
+		JsonArray ret = new JsonArray();
+		while ((line = br.readLine()) != null) {
+			String[] fields = line.split(",");
+
+			JsonElement elem = new JsonObject();
+			elem.getAsJsonObject().add("data", new JsonPrimitive(fields[0]));
+			elem.getAsJsonObject().add("stato", new JsonPrimitive(fields[1]));
+			
+			elem.getAsJsonObject().add("ricoverati_con_sintomi", new JsonPrimitive(fields[2]));
+			elem.getAsJsonObject().add("terapia_intensiva", new JsonPrimitive(fields[3]));
+			elem.getAsJsonObject().add("totale_ospedalizzati", new JsonPrimitive(fields[4]));
+			elem.getAsJsonObject().add("isolamento_domiciliare", new JsonPrimitive(fields[5]));
+			elem.getAsJsonObject().add("totale_positivi", new JsonPrimitive(fields[6]));
+			elem.getAsJsonObject().add("variazione_totale_positivi", new JsonPrimitive(fields[7]));
+			
+			elem.getAsJsonObject().add("nuovi_positivi", new JsonPrimitive(fields[8]));
+			elem.getAsJsonObject().add("dimessi_guariti", new JsonPrimitive(fields[9]));
+			elem.getAsJsonObject().add("deceduti", new JsonPrimitive(fields[10]));
+			elem.getAsJsonObject().add("totale_casi", new JsonPrimitive(fields[11]));
+			elem.getAsJsonObject().add("tamponi", new JsonPrimitive(fields[12]));
+
+			ret.add(elem);
+		}
+
+		br.close();
+
+		return ret;
 	}
 }
--- src/main/java/it/unibo/arces/wot/sepa/tools/covid19/producers/ProtezioneCivile.java
+++ src/main/java/it/unibo/arces/wot/sepa/tools/covid19/producers/ProtezioneCivile.java
@@ -3,13 +3,14 @@ package it.unibo.arces.wot.sepa.tools.covid19.producers;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import com.google.gson.JsonArray;
+
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPABindingsException;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPAPropertiesException;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPAProtocolException;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPASecurityException;
 import it.unibo.arces.wot.sepa.tools.DropGraphs;
 import it.unibo.arces.wot.sepa.tools.covid19.AddObservations;
-import it.unibo.arces.wot.sepa.tools.covid19.AddObservationsCSV;
 
 public class ProtezioneCivile {
 
@@ -19,100 +20,86 @@ public class ProtezioneCivile {
 
 	public static void main(String[] args) throws FileNotFoundException, SEPAProtocolException, SEPASecurityException,
 			SEPAPropertiesException, IOException, SEPABindingsException {
-		DropGraphs agentDropGraphs = new DropGraphs(hostJsap);
-		AddObservations agentObservations = new AddObservations(hostJsap);
-
+		
 		// Drop observation graph
+		DropGraphs agentDropGraphs = new DropGraphs(hostJsap);
 		agentDropGraphs.drop(observationGraph);
 		agentDropGraphs.close();
-
-		// Produce observations
-		agentObservations.addRegionObservations(observationGraph);
-		agentObservations.addProvinceObservations(observationGraph);
+		
+		AddObservations agentObservations = new AddObservations(hostJsap);
+		
+		JsonArray set = AddObservations.loadJsonArray("dpc-covid19-ita-andamento-nazionale-latest.json");
+		
+		agentObservations.addNationalObservations(observationGraph,set);
+		agentObservations.addNationalObservations(historyGraph,set);
+		
+		set = AddObservations.loadJsonArray("dpc-covid19-ita-regioni-latest.json");
+		
+		agentObservations.addRegionObservations(observationGraph,set);
+		agentObservations.addRegionObservations(historyGraph,set);
+		
+		set = AddObservations.loadJsonArray("dpc-covid19-ita-province-latest.json");
+		
+		agentObservations.addProvinceObservations(observationGraph,set);
+		agentObservations.addProvinceObservations(historyGraph,set);
 		
 		agentObservations.close();
 	}
 	
-	protected void produceFromCSV() throws FileNotFoundException, SEPAProtocolException, SEPASecurityException, SEPAPropertiesException, IOException, SEPABindingsException {
+//	public static void main(String[] args) throws FileNotFoundException, SEPAProtocolException, SEPASecurityException,
+//	SEPAPropertiesException, IOException, SEPABindingsException {
+//		updateNationalHistory();
+//	}
+	
+	public static void updateNationalHistory() throws FileNotFoundException, SEPAProtocolException, SEPASecurityException, SEPAPropertiesException, IOException, SEPABindingsException {
 		// Produce history observations
-		String[] arrRegioni = {"dpc-covid19-ita-regioni-20200327.csv",
-				"dpc-covid19-ita-regioni-20200326.csv",
-				"dpc-covid19-ita-regioni-20200325.csv",
-				"dpc-covid19-ita-regioni-20200324.csv",
-				"dpc-covid19-ita-regioni-20200323.csv",
-				"dpc-covid19-ita-regioni-20200322.csv",
-				"dpc-covid19-ita-regioni-20200321.csv",
-				"dpc-covid19-ita-regioni-20200320.csv",
-				"dpc-covid19-ita-regioni-20200319.csv",
-				"dpc-covid19-ita-regioni-20200318.csv",
-				"dpc-covid19-ita-regioni-20200317.csv",
-				"dpc-covid19-ita-regioni-20200316.csv",
-				"dpc-covid19-ita-regioni-20200315.csv",
-				"dpc-covid19-ita-regioni-20200314.csv",
-				"dpc-covid19-ita-regioni-20200313.csv",
-				"dpc-covid19-ita-regioni-20200312.csv",
-				"dpc-covid19-ita-regioni-20200311.csv",
-				"dpc-covid19-ita-regioni-20200310.csv",
-				"dpc-covid19-ita-regioni-20200309.csv",
-				"dpc-covid19-ita-regioni-20200308.csv",
-				"dpc-covid19-ita-regioni-20200307.csv",
-				"dpc-covid19-ita-regioni-20200306.csv",
-				"dpc-covid19-ita-regioni-20200305.csv",
-				"dpc-covid19-ita-regioni-20200304.csv",
-				"dpc-covid19-ita-regioni-20200303.csv",
-				"dpc-covid19-ita-regioni-20200302.csv",
-				"dpc-covid19-ita-regioni-20200301.csv",
+		String[] set = {
+				"dpc-covid19-ita-andamento-nazionale-20200330.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200329.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200328.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200327.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200326.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200325.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200324.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200323.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200322.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200321.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200320.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200319.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200318.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200317.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200316.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200315.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200314.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200313.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200312.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200311.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200310.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200309.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200308.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200307.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200306.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200305.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200304.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200303.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200302.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200301.csv",
 				
-				"dpc-covid19-ita-regioni-20200229.csv",
-				"dpc-covid19-ita-regioni-20200228.csv",
-				"dpc-covid19-ita-regioni-20200227.csv",
-				"dpc-covid19-ita-regioni-20200226.csv",
-				"dpc-covid19-ita-regioni-20200225.csv",
-				"dpc-covid19-ita-regioni-20200224.csv"
+				"dpc-covid19-ita-andamento-nazionale-20200229.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200228.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200227.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200226.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200225.csv",
+				"dpc-covid19-ita-andamento-nazionale-20200224.csv"
 				};
 		
-		String[] arrProvince = {
-				"dpc-covid19-ita-province-20200327.csv",
-				"dpc-covid19-ita-province-20200326.csv",
-				"dpc-covid19-ita-province-20200325.csv",
-				"dpc-covid19-ita-province-20200324.csv",
-				"dpc-covid19-ita-province-20200323.csv",
-				"dpc-covid19-ita-province-20200322.csv",
-				"dpc-covid19-ita-province-20200321.csv",
-				"dpc-covid19-ita-province-20200320.csv",
-				"dpc-covid19-ita-province-20200319.csv",
-				"dpc-covid19-ita-province-20200318.csv",
-				"dpc-covid19-ita-province-20200317.csv",
-				"dpc-covid19-ita-province-20200316.csv",
-				"dpc-covid19-ita-province-20200315.csv",
-				"dpc-covid19-ita-province-20200314.csv",
-				"dpc-covid19-ita-province-20200313.csv",
-				"dpc-covid19-ita-province-20200312.csv",
-				"dpc-covid19-ita-province-20200311.csv",
-				"dpc-covid19-ita-province-20200310.csv",
-				"dpc-covid19-ita-province-20200309.csv",
-				"dpc-covid19-ita-province-20200308.csv",
-				"dpc-covid19-ita-province-20200307.csv",
-				"dpc-covid19-ita-province-20200306.csv",
-				"dpc-covid19-ita-province-20200305.csv",
-				"dpc-covid19-ita-province-20200304.csv",
-				"dpc-covid19-ita-province-20200303.csv",
-				"dpc-covid19-ita-province-20200302.csv",
-				"dpc-covid19-ita-province-20200301.csv",
+		AddObservations agentObservations = new AddObservations(hostJsap);
+		
+		for (String file : set) {
+			JsonArray array = AddObservations.loadCSVNazionale(file);
+			agentObservations.addNationalObservations(historyGraph, array);
+		}
 				
-				"dpc-covid19-ita-province-20200229.csv",
-				"dpc-covid19-ita-province-20200228.csv",
-				"dpc-covid19-ita-province-20200227.csv",
-				"dpc-covid19-ita-province-20200226.csv",
-				"dpc-covid19-ita-province-20200225.csv",
-				"dpc-covid19-ita-province-20200224.csv"
-				};
-		
-		AddObservationsCSV agentObservations = new AddObservationsCSV(hostJsap);
-		
-		for (String province : arrProvince) agentObservations.addProvinceObservations(historyGraph, province);
-		for (String region : arrRegioni) agentObservations.addRegionObservations(historyGraph, region);
-		
 		agentObservations.close();	
 	}
 
