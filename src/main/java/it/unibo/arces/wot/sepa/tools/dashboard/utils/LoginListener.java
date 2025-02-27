cfac90c1ae0b2b970260eebfbf5a8aa91c7d38d5
Luca Roffia <lroffia@gmail.com>
2025-02-27 14:03:33 +0100

    full refactory and merge into sepa project

--- src/main/java/it/unibo/arces/wot/sepa/tools/dashboard/utils/LoginListener.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.arces.wot.sepa.tools.dashboard.utils;
-
-import it.unibo.arces.wot.sepa.commons.response.ErrorResponse;
-
-public interface LoginListener {
-	void onLogin(String id);//, String secret,boolean remember);
-	void onLoginError(ErrorResponse err);
-	void onLoginClose();
-}
